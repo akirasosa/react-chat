4d1b92d497d5d6410bb4e2f5687fe1a89495bc2c
Akira Sosa <akirasosa@gmail.com>
2015-05-20 15:34:14 +0700

    removed unused line

--- webpack.client-watch.js
+++ webpack.client-watch.js
@@ -15,7 +15,6 @@ config.output.hotUpdateMainFilename = "update/[hash]/update.json";
 config.output.hotUpdateChunkFilename = "update/[hash]/[id].update.js";
 
 config.plugins = [
-	new webpack.DefinePlugin({__CLIENT__: true, __SERVER__: false}),
 	new webpack.HotModuleReplacementPlugin(),
 	new webpack.NoErrorsPlugin()
 ];
